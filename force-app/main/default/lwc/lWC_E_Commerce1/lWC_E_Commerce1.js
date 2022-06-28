ca5b57e76ea443d0d7cdfeaaebe3fa0388806fac
Alvise1309 <alviselivotto@gmail.com>
2022-06-28 17:54:40 +0200

    Added method getAssetsOfUser of class AssetUtility

--- force-app/main/default/lwc/lWC_E_Commerce1/lWC_E_Commerce1.js
+++ force-app/main/default/lwc/lWC_E_Commerce1/lWC_E_Commerce1.js
@@ -1,55 +1,51 @@
-import { LightningElement } from 'lwc';
-import{ NavigationMixin} from 'lightning/navigation';
+import { LightningElement, wire } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
+import getAssetsList from '@salesforce/apex/AssetUtility.getAssets';
 
-import myResource from '@salesforce/resourceUrl/myResource';
-import myResource2 from '@salesforce/resourceUrl/myResource2';
-
-export class ButtonMenuMenuItems extends LightningElement {}
+export class ButtonMenuMenuItems extends LightningElement { }
 export default class LWC_E_Commerce1 extends NavigationMixin(LightningElement) {
 
-mR=myResource;
-mR2=myResource2;
+    @wire(getAssetsList) assets;
+
+    Carrello() {
+        var defination = {
+
+            componentDef: "c:carrello",
+            attributes: {
+
+                recordId: '833303'
+            }
 
 
-Carrello(){
-var defination = {
-
-componentDef:"c:carrello",
-attributes:  {
-
-    recordId: '833303'
-}
-
-
-}
-    this[NavigationMixin.Navigate]({
-
-     type : "standard__webPage",
-     attributes: {
-         url:'/one/one.app#' + btoa(JSON.stringify(defination))
-     }
-
-    })
-}
-
-Cellulari(){
-    var defination = {
-    
-    componentDef:"c:cellulari",
-    attributes:  {
-    
-        recordId: '833306'
-    }
-    
-    
-    }
+        }
         this[NavigationMixin.Navigate]({
-    
-         type : "standard__webPage",
-         attributes: {
-             url:'/one/one.app#' + btoa(JSON.stringify(defination))
-         }
-    
+
+            type: "standard__webPage",
+            attributes: {
+                url: '/one/one.app#' + btoa(JSON.stringify(defination))
+            }
+
+        })
+    }
+
+    Cellulari() {
+        var defination = {
+
+            componentDef: "c:cellulari",
+            attributes: {
+
+                recordId: '833306'
+            }
+
+
+        }
+        this[NavigationMixin.Navigate]({
+
+            type: "standard__webPage",
+            attributes: {
+                url: '/one/one.app#' + btoa(JSON.stringify(defination))
+            }
+
         })
     }
 
--- force-app/main/default/lwc/showAssets/showAssets.js
+++
@@ -1,8 +0,0 @@
-import { LightningElement, track, wire } from 'lwc';
-import getAssetsList from '@salesforce/apex/AssetUtility.getAssets';
-
-export default class ShowAssets extends LightningElement {
-
-    @wire(getAssetsList) assets;
-
-}
